42dc62e01648c59f086a40abc16529299f423303
GooDy-PiraTe <anna.sargayeva@mail.ru>
2025-09-24 00:58:47 +0300

    Organising and taking images

--- 06_labs_format_reader.py
+++
@@ -1,8 +0,0 @@
-och, zaoch = 0, 0
-n = int(input())
-for i in range(n):
-    int_i = input(f"int_{i+1}: ").split()  
-    if len(int_i) == 4:
-        if int_i[3] == 'True': och += 1
-        else: zaoch += 1
-print('out:', och, zaoch)
