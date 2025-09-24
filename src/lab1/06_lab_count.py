9203f0452f187f2fbc74e208c92def837b98207d
GooDy-PiraTe <anna.sargayeva@mail.ru>
2025-09-24 16:47:07 +0300

    new tasks done

---
+++ src/lab1/06_lab_count.py
@@ -0,0 +1,12 @@
+n = int(input())
+och, zaoch = 0, 0
+for i in range(n):
+    try:
+        last_name, name, age, format = list(input(f"in_{i+1}: ").split())
+        age = int(age)
+        if format == 'False': zaoch += 1
+        elif format == 'True': och += 1 
+    except: pass
+print(f'out: {och} {zaoch}')
+    #Фамилия Имя Возраст Формат_участия
+
---
+++ src/lab1/07_cipher.py
@@ -0,0 +1,35 @@
+line = input("in: ")
+word_started = False
+origin = []
+step = 0
+f_i = 0
+s_i = 0
+for i in range(len(line)):
+    symb = line[i]
+    if symb.isupper() and not word_started:
+        word_started = True
+        f_i = i
+        origin.append(symb)
+    if word_started and symb.isdigit() and not s_i:
+        s_i = i+1
+        step = s_i - f_i
+    if word_started and step and (i-f_i)%step == 0:
+        origin.append(symb)
+    if word_started and symb == '.':
+        break
+print(f"out: {''.join(origin)}")
+        
+
+'''
+Ht1eadljjl12ojh.
+H t1 e ad l jj l 12 o jh .
+0 12 3 45 6 78 9
+
+hfh H t1e a dl
+012 3 456 7 89
+
+H t1e a dld r
+0 123 4 567 8 
+'''
+        
+
